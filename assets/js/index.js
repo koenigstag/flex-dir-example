1f34755666ff2f0d82ff6b11981b3ee1de12a631
Evhenii Lapshinov <koenigstag@gmail.com>
2021-06-25 13:41:21 +0300

    Fix class order

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,23 +3,23 @@ const container = document.getElementById('flex-container')
 const direction = document.getElementById('direction')
 
 direction.addEventListener('change', ({ target: { value } }) =>
-  container.classList.replace(container.classList.item(2), value)
+  container.classList.replace(container.classList.item(4), value)
 )
 
 const justContent = document.getElementById('just-content')
 
 justContent.addEventListener('change', ({ target: { value } }) =>
-  container.classList.replace(container.classList.item(3), value)
+  container.classList.replace(container.classList.item(5), value)
 )
 
 const alignItems = document.getElementById('align-items')
 
 alignItems.addEventListener('change', ({ target: { value } }) =>
-  container.classList.replace(container.classList.item(4), value)
+  container.classList.replace(container.classList.item(6), value)
 )
 
 const alignContent = document.getElementById('align-content')
 
 alignContent.addEventListener('change', ({ target: { value } }) =>
-  container.classList.replace(container.classList.item(5), value)
+  container.classList.replace(container.classList.item(7), value)
 )
